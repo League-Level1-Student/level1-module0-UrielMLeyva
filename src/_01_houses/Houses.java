93bfaa5178f0982764c93929569cde7f064ca18f
ilabstaff <ilabstaff@172.16.116.73>
2020-02-15 11:25:55 -0800

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,14 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
 		
+		Robot r2d2 = new Robot();
+		r2d2.setPenWidth(40);
+		r2d2.penDown();
+		r2d2.setPenColor(255,0,0);
+		r2d2.move(100);
 	}
 }
